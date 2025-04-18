3d2dddc7e270ae7c5b30e5f1c25e5f58de3eafbd
jwlal <jwlal.dev@gmail.com>
2025-04-18 16:20:21 -0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Add this line to make assets load correctly on GitHub Pages
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
